f2b6df81d99f062b1c1306eadfce40d12c2a92c8
print-alan <a.montiel.work@gmail.com>
2024-10-31 09:14:41 -0600

    Ejercicio 30 - Metodo index

--- Dia3/Dia_3.py
+++ Dia3/Dia_3.py
@@ -11,3 +11,8 @@ print(resultado2)
 frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
 resultado3 = frase.index("práctica")
 print(resultado3)
+
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+resultado4 = frase.rindex("práctica")
+print(resultado4)
+
