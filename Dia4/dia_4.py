db2c6de6d35a2e92bca7bf81c133dee3e88f5c2d
print-alan <a.montiel.work@gmail.com>
2024-11-02 19:00:46 -0600

    Ejercicio 57 - Operadores de comparacion

--- Dia4/dia_4.py
+++ Dia4/dia_4.py
@@ -9,3 +9,9 @@ num1 = 25**0.5 #le sacamos raiz cuadrada al 25 la cual es 5
 num2 = 5
 mi_bool = num1==num2 #comparamos si los valores de num1 y num2 son iguales (Lo cual es verdad  como es de tipo booleano contestara con un True) 
 print(mi_bool)
+
+num1 = 64 * 3
+num2 = 24 * 8
+mi_bool = num1 != num2
+print(f"¿{num1} es diferente de {num2}?") #comparamos si los resultados de num1 y num2 son diferentes lo cual es (False) son iguales
+print(mi_bool)
