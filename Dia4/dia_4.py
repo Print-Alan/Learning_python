0be3d24e71b7a7dd53520543c7af7705c805319d
print-alan <a.montiel.work@gmail.com>
2024-11-02 18:48:10 -0600

    Ejercicio 55 - Operadores de comparacion

---
+++ Dia4/dia_4.py
@@ -0,0 +1,7 @@
+#operadores de comparacion
+
+num1 = 36
+num2 = 17
+mi_bool = num1>=num2 #El operador de comparacion (>=) dice si num1 es mayor o igual a num2 lo cual es cierto es mayor
+print(mi_bool)
+
