cdd4efe097703f7be75aa3c64d6ae405dab9983a
print-alan <a.montiel.work@gmail.com>
2024-11-06 08:44:34 -0600

    Practica 80 - Min y Max

--- Dia4/dia_4.py
+++ Dia4/dia_4.py
@@ -205,3 +205,7 @@ lista_numeros = [44542247/2, 21310/5, 2134747*33, 44556475, 121676, 6654067, 353
 valor_maximo = max(lista_numeros) #el valor maximo de lista de numeros guandalo en valor_maximo
 print(valor_maximo)
 
+lista_numeros = [44542247, 21310, 2134747, 44556475, 121676, 6654067, 353254, 123134, 552512, 611665]
+rango = max(lista_numeros) - min(lista_numeros) #El valor maximo restale el valor minimo y guardo el la variable rango
+print(rango)
+
