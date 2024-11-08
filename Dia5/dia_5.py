be5717ccb9923a9c1f6eb6de22cc533f3ae5319b
print-alan <a.montiel.work@gmail.com>
2024-11-08 09:38:02 -0600

    Ejercicio 89 - Metodos y Ayuda

--- Dia5/dia_5.py
+++ Dia5/dia_5.py
@@ -13,3 +13,9 @@ x = ",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#"
 y = x.lstrip(",:_#") #Los caracteres se van a eliminar, se detendra hasta que encuentre un caracter que no este en la lista
 print(y)
 
+#insert
+#Se utiliza para insertar un elemento en una lista en una posición específica. Este método es muy útil cuando necesitas agregar un elemento en un lugar específico de tu lista sin reemplazar ningún otro elemento.
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+n = frutas.insert(3,"naranja") #primero se coloca la posicion y despues tu item
+print(frutas)
+
