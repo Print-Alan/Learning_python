f01bf3b5c4271187f3f367a597f2f3dc175d262b
print-alan <a.montiel.work@gmail.com>
2024-11-11 12:41:59 -0600

    Ejercicio 100 - Interaccion entre Funciones

--- Dia5/dia_5.py
+++ Dia5/dia_5.py
@@ -170,15 +170,15 @@ def mezclar(lista):
 
 #Pedirle intento
 def probar_suerte():
-    intento = " "
-    while intento not in ["1","2","3","4"]:
-        intento = input("Elige un numero del 1 al 4: ")
+    intento = ""
+    while intento not in ["1","2","3",]:
+        intento = input("Elige un numero del 1 al 3: ")
 
     return int(intento)
 
 #Comprobar intento
 def chequear_intento(lista,intento):
-    if lista[intento - 1] == "-":
+    if lista[intento] == "-":
         print("A lavar los platos")
     else:
         print("Esta vez te has salvado")
@@ -187,3 +187,21 @@ def chequear_intento(lista,intento):
 palitos_mezclados = mezclar(palitos)
 seleccion = probar_suerte()
 chequear_intento(palitos_mezclados,seleccion)
+
+#
+from random import randint
+def lanzar_dados():
+    dado1 = randint(1, 6)
+    dado2 = randint(1, 6)
+    return dado1, dado2
+def evaluar_jugada(dado1, dado2):
+    suma_dados = dado1 + dado2
+    if suma_dados <= 6:
+        return f"La suma de tus dados es {suma_dados}. Lamentable"
+    elif suma_dados > 6 and suma_dados < 10:
+        # elif 6 < suma_dados < 10:
+        return f"La suma de tus dados es {suma_dados}. Tienes buenas chances" 
+    else: return f"La suma de tus dados es {suma_dados}. Parece una jugada ganadora"
+resultado1, resultado2 = lanzar_dados()
+mensaje = evaluar_jugada(resultado1, resultado2)
+print(mensaje)
