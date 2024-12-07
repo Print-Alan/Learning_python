70187552d30f1d71e9333de96edb561d6b1ceb3c
print-alan <a.montiel.work@gmail.com>
2024-12-07 14:43:36 -0600

    Crear y escribir archivos

--- Dia6/dia_6.py
+++ Dia6/dia_6.py
@@ -15,17 +15,31 @@ mi_archivo.close #Cierra el archivo para guardar espacio en memoria, buenas prac
 print(mi_archivo.readline()) #Lee la primera linea del archivo y la imprime
 mi_archivo.close #Cierra el archivo
 
-#
-mi_archivo = open("texto.txt")
+
+#mi_archivo = open("texto.txt")
 print(mi_archivo.read()) #Lee el archivo completo y imprime
 mi_archivo.close
 
-mi_archivo = open("texto.txt")
+#mi_archivo = open("texto.txt")
 print(mi_archivo.readline()) #Lee la primera linea e imprime
 mi_archivo.close
 
-mi_archivo = open("texto.txt")
+#mi_archivo = open("texto.txt")
 mi_archivo.readline() #Lee la primera linea
 print(mi_archivo.readline()) #Lee la segunda linea e imprime
 mi_archivo.close
 
+
+#Crear y escribir archivos
+
+#Vamos a modificar los archivos desde python
+#mi_archivo = open("archivo.txt","r") (Read) Leer es modo por defecto
+#mi_archivo = open("archivo.txt","w") (Write) Escribir recetea todo el contenido del archivo 
+#mi_archivo = open("archivo.txt","a") (append) Se posiona el cursor al final para escribir
+
+archivo = open("prueba.txt","w")
+archivo.write("Soy el nuevo texto\n")
+
+archivo = open("prueba.txt","a")
+archivo.write("Bienvenidos")
+archivo.close()
