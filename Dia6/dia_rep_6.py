c63783df615a4e0c56bdc4790c41797981748d9a
print-alan <a.montiel.work@gmail.com>
2025-01-08 16:03:44 -0600

    Ejercicio 114 - Crear y escribir archivos R

--- Dia6/dia_rep_6.py
+++ Dia6/dia_rep_6.py
@@ -42,3 +42,13 @@ archivo.close()
 archivo = open("mi_archivo.txt")
 print(archivo.read())
 archivo.close()
+
+archivo = open("registro.txt","a")
+registro_ultima_sesion = ["Federico", "20/12/2021", "08:17:32 hs", "Sin errores de carga"]
+for p in registro_ultima_sesion:
+    archivo.writelines(p +"\t")
+archivo.close()
+
+archivo = open("registro.txt")
+print(archivo.read())
+archivo.close()
