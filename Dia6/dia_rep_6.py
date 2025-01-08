26392518135249430fa348f35d32b057e8e35db0
print-alan <a.montiel.work@gmail.com>
2025-01-08 15:31:45 -0600

    Crear y escribir archivos R

--- Dia6/dia_rep_6.py
+++ Dia6/dia_rep_6.py
@@ -9,16 +9,22 @@ print(una_linea)
 
 mi_archivo.close()
 
-mi_archivo = open("texto.txt")
-print(mi_archivo.read())
-mi_archivo.close()
+#mi_archivo = open("texto.txt")
+#print(mi_archivo.read())
+#mi_archivo.close()
 
-mi_archivo = open("texto.txt")
-print(mi_archivo.readline())
-mi_archivo.close()
+#mi_archivo = open("texto.txt")
+#print(mi_archivo.readline())
+#mi_archivo.close()
 
-mi_archivo = open("texto.txt")
-l_por_l = mi_archivo.readline()
-l_por_l = mi_archivo.readline()
-print(l_por_l)
+#mi_archivo = open("texto.txt")
+#l_por_l = mi_archivo.readline()
+#l_por_l = mi_archivo.readline()
+#print(l_por_l)
+#mi_archivo.close()
+
+#Crear y escribir archivos
+
+mi_archivo = open("prueba.txt","w")
+mi_archivo.write("Soy el nuevo texto")
 mi_archivo.close()
